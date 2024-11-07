213fc6a9ef7211d98b8cfc7bf0df27641709ec99
adodoCoder <alavropulido@whccd.edu>
2024-11-06 22:54:42 -0800

    added final parts to js file

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ for(let i=0; i < myNumbers.length; i++){
 
 myNumber.push(6, 7, 8, 9, 10);
 console.log(myNumber);
+
+
+
+console.log(myNumber.pop());
+console.log(myNumber);
+console.log(myNumber.pop());
+console.log(myNumber);
+console.log(myNumber.pop());
+console.log(myNumber);
+
+
+
+
+let myString = "Hello World!";
+console.log(myString.length);
