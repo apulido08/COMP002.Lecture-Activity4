7662f36947af8c52fe99c630374a2c1ca6b5b9e7
adodoCoder <alavropulido@whccd.edu>
2024-11-06 22:35:22 -0800

    modified this page

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let myNumbers = (1, 2, 3, 4, 5);
+let myNumbers = [1, 2, 3, 4, 5];
 console.log(myNumbers[0]);
 console.log(myNumbers[4]);
+
+
+
+//Second Part//
+for(let i=0; i < myNumbers.length; i++){
+    console.log(`myNumber[${i}]: ${myNumber[i]}`);
+}
